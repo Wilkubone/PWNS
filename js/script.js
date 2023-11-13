da9b580b0262e55b6e93faa5089c07aa566101f3
Tomek Wilczynski <wilkubone@gmail.com>
2023-11-13 19:02:58 +0100

    add section about

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const nav = document.querySelector('.navbar')
-    
-    function addShadow () {
-       if (window.scrollY >= 100) {
-        nav.classList.add('shadow-bg')
-       } else {
-        nav.classList.remove('shadow-bg')
-       }
-     }
-     window.addEventListener('scroll', addShadow)
-    
-})
+document.addEventListener("DOMContentLoaded", function () {
+	const nav = document.querySelector(".navbar");
+	const allNavItems = document.querySelectorAll(".nav-link");
+	const navList = document.querySelector(".navbar-collapse");
+
+	function addShadow() {
+		if (window.scrollY >= 100) {
+			nav.classList.add("shadow-bg");
+		} else {
+			nav.classList.remove("shadow-bg");
+		}
+	}
+
+  allNavItems.forEach(item => item.addEventListener('click', () => {
+    navList.classList.remove('show')
+  }))
+  
+	window.addEventListener("scroll", addShadow);
+});
